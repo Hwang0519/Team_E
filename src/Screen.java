0d39689ffa238cda4432989e247d8da70b8ac0a4
rgn73 <rgn73@DESKTOP-9Q3FEAU>
2021-09-14 21:34:39 +0900

    Screen 두번째 업로드

--- src/Screen.java
+++ src/Screen.java
@@ -11,14 +11,16 @@ public class Screen extends Canvas implements KeyListener {
 
 	private int x = 0;
 	private int y = 0;
-	
+	public Car Car[];
 	private BufferedImage image;
 	private static final long serialVersionUID = 1L;
 	
 	public Screen() {
 		try {
 			image = ImageIO.read(new File("src/car.png"));
-			
+			Car = new Car[2];
+			Car[0] = new Car(0,0);
+			Car[1] = new Car(0,50);
 			addKeyListener(this);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -28,7 +30,8 @@ public class Screen extends Canvas implements KeyListener {
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paint(g);
-		g.drawImage(image, x, y, this);
+		g.drawImage(image, Car[0].x, Car[0].y, this);
+		g.drawImage(image, Car[1].x, Car[1].y, this);
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -47,19 +50,24 @@ public class Screen extends Canvas implements KeyListener {
 		switch(e.getKeyCode())
 		{
 		case KeyEvent.VK_UP:
-			y -= gap;
+			Car[0].y -= gap;
+			Car[1].y -= gap;
 			break;
 		case KeyEvent.VK_DOWN:
-			y += gap;
+			Car[0].y += gap;
+			Car[1].y += gap;
 			break;
 		case KeyEvent.VK_LEFT:
-			x -= gap;
+			Car[0].x -= gap;
+			Car[1].x -= gap;
 			break;
 		case KeyEvent.VK_RIGHT:
-			x += gap;
+			Car[0].x += gap;
+			Car[1].x += gap;
 			break;
 		}
-		System.out.println(x+", "+y);
+		System.out.println(Car[0].x+", "+Car[0].y);
+		System.out.println(Car[1].x+", "+Car[1].y);
 		repaint();
 	}
 
